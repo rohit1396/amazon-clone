4e7384cf5fe32173617328900a9a213b8338a2bb
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-17 21:05:35 +0530

    Home Component Added

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img
+        className="home_img"
+        src="\Images\GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Home;
