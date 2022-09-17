1b7fd71d12f73337012d9bd722accda1b6ca277d
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-17 21:02:05 +0530

    Routing Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
       </Routes>
     </div>
   );
