2cf91d6db20dc4e81d540af71ab39ae44d9f21d8
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-20 20:48:57 +0530

    Product Compn

---
+++ src/components/Product.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Product.css";
+import StarIcon from "@mui/icons-material/Star";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import { useStateValue } from "../StateProvider";
+
+const Product = ({ id, title, image, price, rating }) => {
+  const [{}, dispatch] = useStateValue();
+  const addToCart = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+  return (
+    <div className="product">
+      <div className="product_content">
+        <p className="product_title">{title}</p>
+        <p className="product_price">
+          <small>
+            <CurrencyRupeeIcon className="product_priceIcon" />
+          </small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>
+                <StarIcon className="starIcon" />
+              </p>
+            ))}
+        </div>
+      </div>
+      <img className="product_img" src={image} alt="" />
+      <button onClick={addToCart}>Add To Cart</button>
+    </div>
+  );
+};
+
+export default Product;
