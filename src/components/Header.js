f252012f0aa8dcc835afbd994ec98f6138b06ec1
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-20 20:39:23 +0530

    Header Change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,44 +3,46 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { useStateValue } from "../StateProvider";
 
 const Header = () => {
+  const [{ cart }] = useStateValue();
   return (
-    <nav className="header">
+    <nav className="navbar">
       <Link to="/">
         <img
-          className="header_logo"
+          className="navbar_logo"
           src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
           alt="amazon_logo"
         />
       </Link>
-      <div className="header_search">
-        <input className="header_searchInput" type="text" />
-        <SearchIcon className="header_searchIcon" />
+      <div className="navbar_search">
+        <input className="navbar_searchInput" type="text" />
+        <SearchIcon className="navbar_searchIcon" />
       </div>
-      <div className="header_nav">
-        <Link to="/" className="header_link">
-          <div className="header_option">
-            <span className="header_optionLineOne">Hello</span>
-            <span className="header_optionLineTwo">User</span>
+      <div className="navbar_nav">
+        <Link to="/" className="navbar_link">
+          <div className="navbar_option">
+            <span className="navbar_optionOne">Hello</span>
+            <span className="navbar_optionTwo">Sign In</span>
           </div>
         </Link>
-        <Link to="/" className="header_link">
-          <div className="header_option">
-            <span className="header_optionLineOne">Return</span>
-            <span className="header_optionLineTwo">& Orders</span>
+        <Link to="/" className="navbar_link">
+          <div className="navbar_option">
+            <span className="navbar_optionOne">Return</span>
+            <span className="navbar_optionTwo">& Orders</span>
           </div>
         </Link>
-        <Link to="/" className="header_link">
-          <div className="header_option">
-            <span className="header_optionLineOne">Your</span>
-            <span className="header_optionLineTwo">Prime</span>
+        <Link to="/" className="navbar_link">
+          <div className="navbar_option">
+            <span className="navbar_optionOne">Your</span>
+            <span className="navbar_optionTwo">Prime</span>
           </div>
         </Link>
-        <Link to="/" className="header_link">
-          <div className="header_optionBasket">
+        <Link to="/" className="navbar_link">
+          <div className="navbar_optionBasket">
             <ShoppingBasketIcon />
-            <span className="header_basketCount">0</span>
+            <span className="navbar_basketCount">{cart.length}</span>
           </div>
         </Link>
       </div>
