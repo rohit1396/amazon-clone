831ace4eaccd698ce3329659b2d76be4b18e42d8
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-24 22:15:05 +0530

    checkout comp new

---
+++ src/components/Checkout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Checkout.css";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
+const Checkout = () => {
+  const [{ cart }] = useStateValue();
+  return (
+    <div className="checkout">
+      <img
+        className="checkout_advertise_img"
+        src="\Images\ILB_WordCount_NotApproved._CB1613492740_._TTW_.jpg"
+        alt=""
+      />
+      {cart.length == 0 ? (
+        <div>
+          <h2>Your Shopping Cart Is Empty</h2>
+          <p>There is no items available in your shopping cart. </p>
+          <p>
+            Please Add one or more items in your cart to proceed to shopping.
+          </p>
+          <Link to="/">
+            <button>Add Items</button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          {cart.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Checkout;
