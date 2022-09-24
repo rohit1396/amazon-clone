99675d98942e7d13b0a985df5c2a16823b005123
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-24 22:13:54 +0530

    delete functionality

--- src/reducer.js
+++ src/reducer.js
@@ -7,9 +7,30 @@ const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
     case "ADD_TO_CART":
-      return { ...state, cart: [...state.cart, action.item] };
-    case "REMOVE_FROM_CART":
-      return { ...state };
+      return {
+        ...state,
+        cart: [...state.cart, action.item],
+      };
+      break;
+    case "DELETE_ITEM":
+      let newCart = [...state.cart];
+
+      const index = state.cart.findIndex((item) => item.id === action.id);
+
+      if (index >= 0) {
+        newCart.splice(index, 1);
+      } else {
+        console.warn(
+          `cant remove product (id: ${action.id} as its not it the Shopping basket)`
+        );
+      }
+      return {
+        ...state,
+        cart: newCart,
+      };
+      break;
+    default:
+      return state;
   }
 };
 
